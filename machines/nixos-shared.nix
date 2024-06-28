990702a414e23e25cfa45fbe5729ea975cd4b0d2
Diogo Correia <dv_correia@hotmail.com>
2024-06-28 09:47:36 +0100

    Enable SSH for nixos systems

--- machines/nixos-shared.nix
+++ machines/nixos-shared.nix
@@ -111,7 +111,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
