715e6dd2c96c3ecd4d0febcfc50bd9ed58849b61
Diogo Correia <dv_correia@hotmail.com>
2024-06-30 21:09:50 +0100

    Comment to clarify wheel user group

--- users/dvcorreia/nixos.nix
+++ users/dvcorreia/nixos.nix
@@ -4,11 +4,15 @@
     # Since we're using zsh as our shell
     programs.zsh.enable = true;
 
-    # Define a user account. Don't forget to set a password with ‘passwd’.
+    # Define a user account. Don't forget to set a password with ‘passwd’
     users.users.dvcorreia = {
       isNormalUser = true;
       description = "Diogo Correia";
-      extraGroups = [ "docker" "networkmanager" "wheel" ];
+      extraGroups = [
+        "docker"
+        "networkmanager"
+        "wheel" # enable ‘sudo’ for the user
+      ];
       shell = pkgs.zsh;
     };
 
