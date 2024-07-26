a78c3e2368048cc78b7c90afcd5770313feec451
Diogo Correia <dv_correia@hotmail.com>
2024-07-26 18:28:51 +0100

    Update neovim configuration

--- users/dvcorreia/home-manager.nix
+++ users/dvcorreia/home-manager.nix
@@ -186,8 +186,30 @@ in
     enable = true;
 
     extraLuaConfig = ''
+      vim.g.mapleader = ' '
+      vim.g.maplocalleader = ' '
+
       vim.opt.number = true
       vim.opt.relativenumber = true
+
+      -- Share clipboard between OS and Neovim.
+      vim.opt.clipboard = 'unnamedplus'
+
+      vim.opt.breakindent = true
+
+      -- Save undo history.
+      vim.opt.undofile = true
+
+      -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term.
+      vim.opt.ignorecase = true
+      vim.opt.smartcase = true
+
+      -- Minimal number of screen lines to keep above and below the cursor.
+      vim.opt.scrolloff = 10
+
+      -- Set highlight on search, but clear on pressing <Esc> in normal mode
+      vim.opt.hlsearch = true
+      vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
     '';
   };
 
