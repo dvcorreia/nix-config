034b561c4ced612a914d94fcd1dab27fe8430cc5
Diogo Correia <dv_correia@hotmail.com>
2025-02-10 22:49:08 +0000

    fs tweaks to darwin configuration

---
+++ hosts/darwin-shared.nix
@@ -0,0 +1,134 @@
+{ config, pkgs, ... }:
+
+{
+  # We install Nix using a separate installer so we don't want nix-darwin
+  # to manage it for us. This tells nix-darwin to just use whatever is running.
+  nix.useDaemon = true;
+
+  nix = {
+    # We need to enable flakes
+    extraOptions = ''
+      auto-optimise-store = false # not true because of https://github.com/NixOS/nix/issues/7273
+      experimental-features = nix-command flakes
+      extra-platforms = x86_64-darwin
+      sandbox = true
+    '';
+
+    gc = {
+      # user = "root";
+      automatic = true;
+      options = "--delete-older-than 14d";
+    };
+  };
+
+  # zsh is the default shell on Mac and we want to make sure that we're
+  # configuring the rc correctly with nix-darwin paths.
+  programs.zsh.enable = true;
+
+  environment.shells = with pkgs; [
+    bashInteractive
+    zsh
+  ];
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+  system.activationScripts.postUserActivation.text = ''
+    # activateSettings -u will reload the settings from the database and apply them to the current session,
+    # so we do not need to logout and login again to make the changes take effect.
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
+
+  system.defaults = {
+    menuExtraClock.Show24Hour = true;  # show 24 hour clock
+
+    dock = {
+      autohide = false;
+      mru-spaces = false;
+      orientation = "bottom";
+      showhidden = true;
+      tilesize = 44;
+      mineffect = "scale";
+      # persistent-apps = [
+      #   ""
+      # ];
+      show-recents = false;
+
+      wvous-tl-corner = 2; # top-left - Mission Control
+      wvous-tr-corner = 13; # top-right - Lock Screen
+      wvous-bl-corner = 3; # bottom-left - Application Windows
+      wvous-br-corner = 4; # bottom-right - Desktop
+    };
+
+    finder = {
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
+      QuitMenuItem = true;
+      ShowStatusBar = true;
+      ShowPathbar = true;
+      FXEnableExtensionChangeWarning = false;
+      FXDefaultSearchScope = "SCcf"; # when performing a search, search the current folder by default
+      FXPreferredViewStyle = "Nlsv"; # use list view in all Finder windows by default
+    };
+
+    trackpad = {
+      TrackpadRightClick = true;  # enable two finger right click
+      TrackpadThreeFingerDrag = false;  # disable three finger drag
+    };
+
+    NSGlobalDomain = {
+      "com.apple.swipescrolldirection" = true;  # enable natural scrolling(default to true)
+      AppleInterfaceStyle = "Dark";  # dark mode
+      AppleKeyboardUIMode = 3;  # Mode 3 enables full keyboard control.
+      ApplePressAndHoldEnabled = true;  # enable press and hold
+
+      # sets how long it takes before it starts repeating.
+      InitialKeyRepeat = 15;  # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+      # sets how fast it repeats once it starts.
+      KeyRepeat = 3;  # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+
+      NSAutomaticCapitalizationEnabled = false;  # disable auto capitalization
+      NSAutomaticDashSubstitutionEnabled = false;  # disable auto dash substitution
+      NSAutomaticPeriodSubstitutionEnabled = false;  # disable auto period substitution
+      NSAutomaticQuoteSubstitutionEnabled = false;  # disable auto quote substitution
+      NSAutomaticSpellingCorrectionEnabled = false;  # disable auto spelling correction
+      NSNavPanelExpandedStateForSaveMode = true;  # expand save panel by default
+      NSNavPanelExpandedStateForSaveMode2 = true;
+    };
+
+    CustomUserPreferences = {
+      NSGlobalDomain = {
+        AppleAccentColor = "-1"; # Graphite
+        AppleHighlightColor = "0.847059 0.847059 0.862745 Graphite";
+      };
+
+      "com.apple.frameworks.diskimages" = {
+        # automatically open a new Finder window when a volume is mounted
+        "auto-open-ro-root" = true;
+        "auto-open-rw-root" = true;
+      };
+
+      "com.apple.desktopservices" = {
+        # avoid creating .DS_Store files on network or USB volumes
+        DSDontWriteNetworkStores = true;
+        DSDontWriteUSBStores = true;
+      };
+
+      "com.apple.finder".WarnOnEmptyTrash = false;
+      "com.apple.finder".OpenWindowForNewRemovableDisk = true; # not working!
+    };
+
+    screencapture = {
+      location = "~/Desktop";
+      type = "png";
+      disable-shadow = true;
+    };
+
+    screensaver = {
+      # require password immediately after sleep or screen saver begins
+      askForPassword = true;
+      askForPasswordDelay = 0;
+    };
+  };
+}
