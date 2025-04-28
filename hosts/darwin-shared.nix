e160b383537d3842fd0431a1bf81bee290599d4f
Diogo Correia <dv_correia@hotmail.com>
2025-04-28 17:58:46 +0200

    setup mac wallpaper

--- hosts/darwin-shared.nix
+++ hosts/darwin-shared.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
+let
+  wallpaper = ./wallpaper.jpg;
+in
 {
   nix = {
     settings = {
@@ -31,6 +34,14 @@
 
   # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
   system.activationScripts.postUserActivation.text = ''
+    # setup desktop wallpaper
+    osascript -e '
+      set desktopImage to POSIX file "${wallpaper}"
+      tell application "Finder"
+        set desktop picture to desktopImage
+      end tell
+    '
+
     # activateSettings -u will reload the settings from the database and apply them to the current session,
     # so we do not need to logout and login again to make the changes take effect.
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
