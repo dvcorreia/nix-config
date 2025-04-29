d156c1e03a4c17cdf818d5e3507bff3c76e425aa
Diogo Correia <dv_correia@hotmail.com>
2025-04-29 18:58:05 +0100

    add proart-7950x public key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,11 @@ let
   users = [ dvcorreia ];
 
   macbook-m3-pro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIsWETol5xHJFEZi1v+H1MVCpXnvpm0iIZd3VEmjldSo root@macbook-m3-pro";
-  systems = [ macbook-m3-pro ];
+  proart-7950x = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwdsk5RrvGq/QOVASenS4SWR6lTjr9gXPpivcjCijHl root@proart-7950x";
+  systems = [
+    macbook-m3-pro
+    proart-7950x
+  ];
 in
 {
   "secret1.age".publicKeys = users ++ systems;
