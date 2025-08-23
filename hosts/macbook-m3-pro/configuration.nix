cff869142c9596e203a0d31fd4f4b57cdebfe09a
Diogo Correia <dv_correia@hotmail.com>
2025-08-23 21:53:42 +0100

    feat(macbook-m3-pro): enable ollama

--- hosts/macbook-m3-pro/configuration.nix
+++ hosts/macbook-m3-pro/configuration.nix
@@ -1,5 +1,13 @@
-_: {
-  imports = [
+{
+  inputs,
+  ...
+}:
+
+{
+  imports = with inputs.self.nixosModules; [
     ../darwin-shared.nix
+    services-ollama
   ];
+
+  services.ollama.enable = true;
 }
