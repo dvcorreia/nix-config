4e600e052bd4fadb98d3ddbf81e8162823ab88b7
Diogo Correia <dv_correia@hotmail.com>
2026-03-10 22:54:21 +0000

    chore: deprecate infra dev shell

--- flake.nix
+++ flake.nix
@@ -49,6 +49,7 @@
       home-manager,
       darwin,
       agenix,
+      agenix-shell,
       ...
     }@inputs:
     let
@@ -132,20 +133,35 @@
           pkgs = nixpkgsFor.${system};
         in
         {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              git
-              gnumake
-              agenix.packages.${system}.default
-              nixos-rebuild
-            ];
-          };
+          default =
+            let
+              installationScript = agenix-shell.lib.installationScript system {
+                secrets = {
+                  TF_VAR_hcloud_token.file = ./secrets/hetzner-homelab-api-token.age;
+                  TF_VAR_cloudflare_api_token.file = ./secrets/cloudflare-dns-token.age;
+                  TF_VAR_passphrase.file = ./secrets/opentofu-encryption-key.age;
+                };
+              };
+            in
+            pkgs.mkShell {
+              packages = with pkgs; [
+                git
+                gnumake
+                openssh
+                opentofu
+                agenix.packages.${system}.default
+                nixos-rebuild
+              ];
 
-          infra = import ./shells/infra.nix {
-            inherit pkgs system inputs;
-          };
+              shellHook = ''
+                source ${pkgs.lib.getExe installationScript}
+              '';
+
+              TF_VAR_ssh_pub_key = sshKeys.yubikey1-ed25519-sk;
+              TF_VAR_cloudflare_zone_id = "6f9c7fc4fe11ede15a136982bedcad85";
+              TF_VAR_cloudflare_account_id = "35db5f9742873a380407761666ef726b";
+            };
         }
       );
-
     };
 }
