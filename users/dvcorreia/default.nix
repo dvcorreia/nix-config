64a52c852154e2f0f6e5777da01678db728a967d
Diogo Correia <dv_correia@hotmail.com>
2026-05-29 23:43:26 +0100

    fix(vscode): set pierre icons and theme

--- users/dvcorreia/default.nix
+++ users/dvcorreia/default.nix
@@ -260,6 +260,18 @@ in
             version = "2.1.0";
             sha256 = "sha256-WBMBj0ZBHVf6wDuXoNgkvDdDZZZLtaRipydmO7x9DP4=";
           }
+          {
+            name = "pierre-theme";
+            publisher = "pierrecomputer";
+            version = "1.0.3";
+            sha256 = "sha256-NI7wxBu8xFoepgY6bc85ofLOjg0F9gthmT1LJm8ZcJk=";
+          }
+          {
+            name = "pierre-vscode-icons";
+            publisher = "pierrecomputer";
+            version = "0.0.9";
+            sha256 = "sha256-m2bWwKllNQ4+aG7nUHCd+7LTXgQ0QmSBZxHvG3I3i2A=";
+          }
         ];
 
       userSettings = {
@@ -276,6 +288,10 @@ in
         "workbench.startupEditor" = "none";
         "workbench.sideBar.location" = "right";
         "[nix]"."editor.tabSize" = 2;
+        "window.autoDetectColorScheme" = true;
+        "workbench.preferredDarkColorTheme" = "Pierre Dark";
+        "workbench.preferredLightColorTheme" = "Pierre Light";
+        "workbench.iconTheme" = "pierre-icons-complete";
       };
     };
   };
